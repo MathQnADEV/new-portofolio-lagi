f42d09514d9ea127b66d214e35dc98845de11886
MathQnADEV <mathqna.project@gmail.com>
2022-11-21 11:04:57 +0800

    baru

--- pages/components/Dashboards.js
+++ pages/components/Dashboards.js
@@ -7,6 +7,7 @@ import {
   } from "react-icons/ai";
 import Image from "next/image";
 import deved from "../../public/kane-no-backg.png";
+import Typed from "react-typed"
 
 const Dashboards = () => {
     return(
@@ -14,15 +15,24 @@ const Dashboards = () => {
             <h2 className="text-teal-500 text-5xl py-2 color font-medium md:text-6xl text-center ml-2">
                 Shaquille Rashaun
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl">
+            <div className="text-2xl py-5 md:text-3xl">
+            <h3 className="mb-4">
                 Fullstack Web Developer
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">
+            <Typed
+          className='md:text-3xl sm:text-2xl text-2xl font-bold md:pl-4 pl-2 pt-2 '
+            strings={['Javascript', 'React', 'Next JS', 'Tailwind']}
+            typeSpeed={140}
+            backSpeed={100}
+            loop
+          />
+            </div>
+            <p className="text-md leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">
                 Freelancer yang menyediakan layanan untuk kebutuhan content
                 pemograman Fullstack Web Developer. Bergabunglah dengan saya di
                 bawah ini and let's get CRACKING!
             </p>
-        </div><div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white">
+        </div><div className="text-5xl flex justify-center gap-16 pt-5 pb-2s text-gray-600 dark:text-white">
                 <a
                     href="https://twitter.com/MathqnaDEV"
                     target="_blank"
@@ -44,7 +54,7 @@ const Dashboards = () => {
                 >
                     <AiFillInstagram />
                 </a>
-            </div><div className="realtive mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:w-96 md:h-96">
+            </div><div className="realtive mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-10 overflow-hidden md:w-96 md:h-96">
                 <Image
                     src={deved}
                     className="w-80 h-80 md:w-96 md:h-96 object-cover ml-3" />
